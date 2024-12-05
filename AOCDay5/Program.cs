3f02fdbd78571d0714f5fe8895506b10ec90deaa
Jeremy Mann <jeremy.richard.mann@gmail.com>
2024-12-05 14:37:24 -0600

    Day2 still broken

--- AOCDay5/Program.cs
+++ AOCDay5/Program.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 class AOCDay5
 {
     private Dictionary<string, List<int>> Map;
-    private static readonly string path = @"C:\Users\jerem\OneDrive\Documents\Advent2024\advent2024\AOCDay5\inputsmall.txt";
+    private static readonly string path = @"C:\Users\jerem\OneDrive\Documents\Advent2024\advent2024\AOCDay5\input.txt";
     private List<string> puzzle;
     private List<string> invalidPuzzle;
     private string[] Data;
     public AOCDay5()
     {
-        Map = new Dictionary<string, List<int>>();
-        puzzle = new List<string>();
-        invalidPuzzle = new List<string>();
+        Map = [];
+        puzzle = [];
+        invalidPuzzle = [];
     }
     public void LoadData()
     {
@@ -35,7 +35,7 @@ class AOCDay5
                 k = line.TrimEnd().Split('|')[0];
                 v = int.Parse(line.TrimEnd().Split('|')[1]);
 
-                if (Map.Keys.Contains(k))
+                if (Map.ContainsKey(k))
                 {
                     Map[k].Add(v);
                 }
@@ -59,9 +59,8 @@ class AOCDay5
         {
             foreach (string compare in puzAry[(i + 1)..])
             {
-                if (!Map.Keys.Contains(puzAry[i]) && i < puzAry.Length - 1)
+                if (!Map.ContainsKey(puzAry[i]) && i < puzAry.Length - 1)
                 {
-                    
                     invalidPuzzle.Add(s);
                     return false;
                 }
@@ -95,7 +94,40 @@ class AOCDay5
 
     public int Part2()
     {
-       return 0;
+        int Total = 0;
+        foreach(string puz in invalidPuzzle)
+        {
+            List<string> newAry = [];
+            string[] puzAry = puz.Split(',');
+            for(int i = puzAry.Length - 1; i >= 0; i--)
+            {
+                if(i == puzAry.Length - 1)
+                {
+                    newAry.Add(puzAry[i]);
+                }
+                else
+                {
+                    List<string> prevAry = new List<string>(newAry);
+                    foreach (string entry in prevAry)
+                    {
+                        if (Map.ContainsKey(puzAry[i]) && Map[puzAry[i]].Contains(int.Parse(entry)))
+                        {
+                            newAry.Insert(0, puzAry[i]);
+                            break;
+                        }
+                    }
+                    if(!newAry.Contains(puzAry[i]))
+                    {
+                        newAry.Add(puzAry[i]);
+                    }
+                }
+            }
+            Console.WriteLine("Puz: {0}; NewPuz: {1}", puz, String.Join(',', newAry));
+            decimal d = Convert.ToDecimal(newAry.Count / 2.0);
+            int Half = int.Parse(Math.Ceiling(d).ToString());
+            Total += int.Parse(newAry[Half]);
+        }
+       return Total;
     }
     static void Main()
     {
