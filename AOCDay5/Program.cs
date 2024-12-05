d0013f2fd22c65a2271887cecb126cdd0fa14666
Jeremy Mann <jeremy.richard.mann@gmail.com>
2024-12-05 09:48:21 -0600

    Day5

---
+++ AOCDay5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+class AOCDay5
+{
+    private Dictionary<string, int[]> Map;
+    private static readonly string path = @"C:\Users\jerem\OneDrive\Documents\Advent2024\advent2024\AOCDay5\inputsmall.txt";
+    private string[] Data;
+    public void LoadData()
+    {
+        Data = File.ReadAllLines(path);
+        bool isPuzzle = false;
+        int i = 0;
+        foreach (string line in Data)
+        {
+            if('\n'.Equals(line))
+            {
+                // Map is done, start filling the puzzle entity
+                isPuzzle = true;
+            }
+            if(!isPuzzle)
+            {
+
+            }
+            else
+            {
+                // Build Map
+                string k;
+                int v;
+                k = line.TrimEnd().Split('|')[0];
+                v = int.Parse(line.TrimEnd().Split('|')[1]);
+
+                if (Map.Keys.Contains(k))
+                {
+                    Map[k].Append(v);
+                }
+                else
+                {
+                    Map.Add(k, [v]);
+                }
+            }
+        }
+    }
+    static void Main()
+    {
+
+    }
+}
