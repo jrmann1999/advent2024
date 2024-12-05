360d5a5a359924f30f5f15bb71d44529ae26c08c
Jeremy Mann <jeremy.richard.mann@gmail.com>
2024-12-05 13:47:44 -0600

    Part1 Complete

--- AOCDay5/Program.cs
+++ AOCDay5/Program.cs
@@ -1,27 +1,33 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Net;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 class AOCDay5
 {
-    private Dictionary<string, int[]> Map;
+    private Dictionary<string, List<int>> Map;
     private static readonly string path = @"C:\Users\jerem\OneDrive\Documents\Advent2024\advent2024\AOCDay5\inputsmall.txt";
+    private List<string> puzzle;
+    private List<string> invalidPuzzle;
     private string[] Data;
+    public AOCDay5()
+    {
+        Map = new Dictionary<string, List<int>>();
+        puzzle = new List<string>();
+        invalidPuzzle = new List<string>();
+    }
     public void LoadData()
     {
         Data = File.ReadAllLines(path);
         bool isPuzzle = false;
-        int i = 0;
         foreach (string line in Data)
         {
-            if('\n'.Equals(line))
+            if ('\n'.Equals(line) || "".Equals(line))
             {
                 // Map is done, start filling the puzzle entity
                 isPuzzle = true;
+                continue;
             }
-            if(!isPuzzle)
-            {
-
-            }
-            else
+            if (!isPuzzle)
             {
                 // Build Map
                 string k;
@@ -31,17 +37,91 @@ class AOCDay5
 
                 if (Map.Keys.Contains(k))
                 {
-                    Map[k].Append(v);
+                    Map[k].Add(v);
                 }
                 else
                 {
                     Map.Add(k, [v]);
                 }
             }
+            else
+            {
+                //Build Puzzle
+                puzzle.Add(line);
+            }
         }
+    }
+
+    private bool isGood(string s)
+    {
+        string[] puzAry = s.Split(',');
+        for (int i = 0; i < puzAry.Length; i++)
+        {
+            foreach (string compare in puzAry[(i + 1)..])
+            {
+                if (!Map.Keys.Contains(puzAry[i]) && i < puzAry.Length - 1)
+                {
+                    
+                    invalidPuzzle.Add(s);
+                    return false;
+                }
+                else if (!Map[puzAry[i]].Contains(int.Parse(compare)))
+                {
+                    invalidPuzzle.Add(s);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private bool isGood2(string s)
+    {
+        string[] puzAry = s.Split(',');
+        for (int i = 0; i < puzAry.Length; i++)
+        {
+            foreach (string compare in puzAry[(i + 1)..])
+            {
+                if (!Map.Keys.Contains(puzAry[i]) && i < puzAry.Length - 1)
+                {
+                    
+                    return false;
+                }
+                else if (!Map[puzAry[i]].Contains(int.Parse(compare)))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public int Part1()
+    {
+        int Total = 0;
+        bool isValid = true;
+        foreach (string puz in puzzle)
+        { 
+            if (isGood(puz))
+            {
+                //Console.WriteLine(puz);
+                string[] puzAry = puz.Split(',');
+                decimal d = puzAry.Length / 2;
+                int Half = int.Parse(Math.Ceiling(d).ToString());
+                Total += int.Parse(puzAry[Half]);
+            }
+        }
+        return Total;
+    }
+
+    public int Part2()
+    {
+       return 0;
     }
     static void Main()
     {
-
+        var AOC = new AOCDay5();
+        AOC.LoadData();
+        Console.WriteLine("Part1: {0}", AOC.Part1());
+        Console.WriteLine("Part2: {0}", AOC.Part2());
     }
 }
